18c308e64efb005e83eb501cad35ad75d0038ac1
Indicado <indicado@walrus.gg>
2020-04-23 12:36:33 +0200

    Set presence every 15 minutes

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -11,13 +11,15 @@ export default class ReadyListener extends Listener {
 
   public async exec(): Promise<void> {
     this.client.logger.info(`Logged in as ${this.client.user.tag}`);
-    this.client.user.setPresence({
-      activity: {
-        name: `${process.env.PREFIX}help | ${this.client.guilds.cache.size} servers | ${this.client.users.cache.size} users`,
-        type: "PLAYING",
-      },
-      status: "online",
-    });
+    setInterval(() => {
+      this.client.user.setPresence({
+        activity: {
+          name: `${process.env.PREFIX}help | ${this.client.guilds.cache.size} servers | ${this.client.users.cache.size} users`,
+          type: "PLAYING",
+        },
+        status: "online",
+      });
+    }, 900000);
 
     if (this.client.voice.connections.size > 0)
       this.client.voice.connections.map((c) => {
